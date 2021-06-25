df62b570e19e13fb69b3693eebcd0b875eacfd72
Akeem C <javantieclarke@gmail.com>
2021-06-25 14:39:53 -0400

    updated save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,85 @@
-var currentDayDisplayEl = $(".currentDay");
+var task = {};
 
-var currentTime = new Date();
-console.log(currentTime);
+///  Code to display current day
+var rightNow = moment().format("dddd, h:mm A");
+console.log(rightNow);
+$("#currentDay").append(rightNow);
 
-currentDayDisplayEl
+//Save the data to localstorage
+$(".saveBtn").click(function () {
+  var textAreaEl = $("textarea").val();
+  console.log(textAreaEl);
+  localStorage.setItem("taskInfo", JSON.stringify(textAreaEl));
+});
 
+//Load the data from localstorage
+var loadTask = function () {
+  task = JSON.parse(localStorage.getItem("taskInfo"));
+
+  if (!task) {
+    return;
+  }
+  $("textarea").text(task);
+  console.log(task);
+};
+loadTask();
+
+//var scheduleTImeEl = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
+//   for (let i = 0; i < scheduleTImeEl.length; i++) {
+//     if(scheduleTImeEl.note[i]){
+//         console.log("index is", i)
+//         console.log("index is",scheduleTImeEl[i])
+//     }
+
+//   }
+
+var timeBlock = [
+  {
+    time: "9:00",
+    note: "",
+  },
+  {
+    time: "10:00",
+    note: "",
+  },
+  {
+    time: "11:00",
+    note: "",
+  },
+  {
+    time: "12:00",
+    note: "",
+  },
+  {
+    time: "1:00",
+    note: "",
+  },
+  {
+    time: "2:00",
+    note: "",
+  },
+  {
+    time: "3:00",
+    note: "",
+  },
+  {
+    time: "4:00",
+    note: "",
+  },
+  {
+    time: "5:00",
+    note: "",
+  },
+];
+
+
+for (let i = 0; i < timeBlock.length; i++) {
+  if (timeBlock[i].note) {
+    console.log("index is", i);
+    console.log("index is", timeBlock[i]);
+  }
+//   var newEl = document.createElement("tr");
+//   newEl.textContent = timeBlock[i].time;
+}
+
+//var striningfield = JSON.stringify(scheduleTImeEl);
