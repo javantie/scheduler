545b7d6ea687722220c2e42484fd43d3140d0822
Akeem C <javantieclarke@gmail.com>
2021-06-26 16:01:01 -0400

    updated save btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-var task = {};
+var task = []
+
 
 ///  Code to display current day
 var rightNow = moment().format("dddd, h:mm A");
@@ -8,76 +9,100 @@ $("#currentDay").append(rightNow);
 //Save the data to localstorage
 $(".saveBtn").click(function () {
   var oldTask = [];
-  var newTask = $("textarea").val();
-  console.log(newTask);
-  oldTask.push(newTask);
-  localStorage.setItem("taskInfo", JSON.stringify(oldTask));
+  console.log($(this).parent().siblings("div").children()[0])
+  var textValEl = $(this).parent().siblings("div").children()[0];
+  console.log($(textValEl).attr("id"));
+  task.push({
+    id: $(textValEl).attr("id"),
+    value: $(textValEl).val()
+  });
+  console.log(task)
+  localStorage.setItem("taskInfo", JSON.stringify(task));
 });
 
 //Load the data from localstorage
 var loadTask = function () {
-  task = JSON.parse(localStorage.getItem("taskInfo"));
-  if (!task) {
-    return;
+  task = JSON.parse(localStorage.getItem("taskInfo")) || [];
+  
+$("textarea").each(function(index, element){
+  console.log(element)
+  for(var i = 0; i < task.length; i++){
+    if ($(element).attr("id") ===task[i].id){
+      $(element).val(task[i].value)
+    }
   }
-  $("textarea").text(task);
+  })
   console.log(task);
 };
 loadTask();
 
 
 
-var TimeEl = [];
 
-for (let i = 9; i < 8; i++) {
-  var newObj = {
-    time:"i",
-    note: ""
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var TimeEl = [];
+
+// for (let i = 9; i < 8; i++) {
+//   var newObj = {
+//     time:"i",
+//     note: ""
+//   }
   
-}
+// }
 
     
 
 
-var timeBlock = [
-  {
-    time: "9:00",
-    note: "",
-  },
-  {
-    time: "10:00",
-    note: "",
-  },
-  {
-    time: "11:00",
-    note: "",
-  },
-  {
-    time: "12:00",
-    note: "",
-  },
-  {
-    time: "1:00",
-    note: "",
-  },
-  {
-    time: "2:00",
-    note: "",
-  },
-  {
-    time: "3:00",
-    note: "",
-  },
-  {
-    time: "4:00",
-    note: "",
-  },
-  {
-    time: "5:00",
-    note: "",
-  },
-];
+// var timeBlock = [
+//   {
+//     time: "9:00",
+//     note: "",
+//   },
+//   {
+//     time: "10:00",
+//     note: "",
+//   },
+//   {
+//     time: "11:00",
+//     note: "",
+//   },
+//   {
+//     time: "12:00",
+//     note: "",
+//   },
+//   {
+//     time: "1:00",
+//     note: "",
+//   },
+//   {
+//     time: "2:00",
+//     note: "",
+//   },
+//   {
+//     time: "3:00",
+//     note: "",
+//   },
+//   {
+//     time: "4:00",
+//     note: "",
+//   },
+//   {
+//     time: "5:00",
+//     note: "",
+//   },
+// ];
 
 
 // for (let i = 0; i < timeBlock.length; i++) {
